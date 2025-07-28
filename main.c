2475915407960ed55e828386b4e6ad86f6846044
shahd-moh-abdel <shahd.mohammed.abdel@gmail.com>
2025-07-28 16:38:53 +0300

    added grid

--- main.c
+++ main.c
@@ -1,17 +1,47 @@
+
 #include "raylib.h"
+
+#define SCREEN_WIDTH 480
+#define SCREEN_HEIGHT 480
+#define GRID_SIZE 16
+#define CELL_SIZE 30
 
 int main (void)
 {
-  const int screen_width = 480;
-  const int screen_height = 480;
+  
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "GIF maker");
 
-  InitWindow(screen_width, screen_height, "GIF maker");
+  Color grid[GRID_SIZE][GRID_SIZE];
+
+  for(int y = 0; y < GRID_SIZE; y++)
+    {
+      for(int x = 0; x < GRID_SIZE; x++)
+	{
+	  grid[x][y] = WHITE;
+	}
+      
+    }
+  
   SetTargetFPS(60);
 
   while(!WindowShouldClose())
     {
       BeginDrawing();
-      ClearBackground(WHITE);
+      ClearBackground(BLACK);
+
+      
+      for(int y = 0; y < GRID_SIZE; y++)
+	{
+	  for(int x = 0; x < GRID_SIZE; x++)
+	    {
+	      DrawRectangle(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE - 1, CELL_SIZE - 1, grid[y][x] );
+	    }
+      
+	}
+  
+
+                 
+      
       EndDrawing();
     }
 
