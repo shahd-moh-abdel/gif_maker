4ce929700458f023942e62af5fd3f4bb4859974a
shahd-moh-abdel <shahd.mohammed.abdel@gmail.com>
2025-07-30 00:01:16 +0300

    added a side panal

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 
 #include "raylib.h"
 
-#define SCREEN_WIDTH 480
+#define SCREEN_WIDTH 780
 #define SCREEN_HEIGHT 480
 #define GRID_SIZE 16
 #define MAX_COLOR_COUNT 24
@@ -18,7 +18,7 @@ int main (void)
   
   
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "GIF maker");
-  int CELL_SIZE = SCREEN_WIDTH / GRID_SIZE;
+  int CELL_SIZE = SCREEN_HEIGHT / GRID_SIZE;
 
   
 
@@ -35,18 +35,29 @@ int main (void)
   
   SetTargetFPS(60);
 
+  RenderTexture2D canvas = LoadRenderTexture(SCREEN_WIDTH - 300, SCREEN_HEIGHT);
+
   while(!WindowShouldClose())
     {
       Vector2 mouse_pos = GetMousePosition();
-      int grid_x = mouse_pos.x / CELL_SIZE;
-      int grid_y = mouse_pos.y / CELL_SIZE;
 
-      if(IsMouseButtonDown(MOUSE_LEFT_BUTTON)){
-	  grid[grid_y][grid_x] = colors[color_selected];
+
+      
+      if(
+	 mouse_pos.x > 300 && mouse_pos.x < SCREEN_WIDTH
+	 && mouse_pos.y > 0 && mouse_pos.y < SCREEN_HEIGHT){
+	
+	int grid_x = (mouse_pos.x - 300) / CELL_SIZE;
+	int grid_y = mouse_pos.y / CELL_SIZE;
+	
+	if(IsMouseButtonDown(MOUSE_LEFT_BUTTON)){
+	  grid[grid_x][grid_y] = colors[color_selected];
+	}
+	if(IsMouseButtonDown(MOUSE_RIGHT_BUTTON)){
+	  grid[grid_x][grid_y] = WHITE;
+	}
       }
-      if(IsMouseButtonDown(MOUSE_RIGHT_BUTTON)){
-	grid[grid_y][grid_x] = WHITE;
-      }
+
       if(IsKeyPressed(KEY_C)){
 	for(int y = 0; y < GRID_SIZE; y++)
 	  {
@@ -64,22 +75,29 @@ int main (void)
 
       if (color_selected >= MAX_COLOR_COUNT) color_selected = MAX_COLOR_COUNT - 1 ;
       else if (color_selected < 0) color_selected = 0;
-      
-      BeginDrawing();
-      ClearBackground(WHITE);
 
+      BeginTextureMode(canvas);
       
       for(int y = 0; y < GRID_SIZE; y++)
 	{
 	  for(int x = 0; x < GRID_SIZE; x++)
 	    {
-	      DrawRectangle(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE, grid[y][x] );
+	      DrawRectangle(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE, grid[x][y] );
+
 	    }
       
 	}
-  
+ 
 
-                 
+      EndTextureMode();   
+
+      
+      BeginDrawing();
+      ClearBackground(SKYBLUE);
+
+      DrawTextureRec(canvas.texture, (Rectangle){0, 0, (float)canvas.texture.width, (float)-canvas.texture.height},(Vector2){300, 0}, WHITE);
+
+
       
       EndDrawing();
     }
