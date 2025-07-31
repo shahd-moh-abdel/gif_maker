9b9b3d9b3765c6c8bb06a9eb87f77cf107b80e26
shahd-moh-abdel <shahd.mohammed.abdel@gmail.com>
2025-07-31 23:14:28 +0300

    used ffmpeg to export frames into gif

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <gif_lib.h>
+#include <stdio.h>
 #include "raylib.h"
 
 #define SCREEN_WIDTH 780
 #define SCREEN_HEIGHT 480
-#define GRID_SIZE 8
+#define GRID_SIZE 16
 #define MAX_COLOR_COUNT 24
 #define MAX_FRAMES 12
 
@@ -14,14 +15,53 @@ typedef struct {
   Color grid[GRID_SIZE][GRID_SIZE];
 } Frame;
 
-Color colors[MAX_COLOR_COUNT] = {
-  LIGHTGRAY,GRAY,DARKGRAY,YELLOW, GOLD, ORANGE, PINK, RED, MAROON, GREEN, LIME, DARKGREEN, SKYBLUE,BLUE, DARKBLUE, PURPLE, VIOLET, DARKPURPLE, BEIGE, BROWN, DARKBROWN, WHITE, BLACK, MAGENTA
-};
 
-//void save_as_gif();
+
+void save_frames_as_pngs(Frame* frames, int frame_count, int cell_size) {
+  for(int i = 0; i < frame_count; i++){
+    Image frame_image = {
+      .data = frames[i].grid,
+      .width = GRID_SIZE,
+      .height = GRID_SIZE,
+      .format = PIXELFORMAT_UNCOMPRESSED_R8G8B8A8,
+      .mipmaps = 1
+    };
+
+     Image scaled_image = ImageCopy(frame_image);
+    ImageResizeNN(&scaled_image, GRID_SIZE * cell_size, GRID_SIZE * cell_size);
+    char file_name[256];
+
+    sprintf(file_name, "temp/frame_%03d.png", i);
+    ExportImage(scaled_image, file_name);
+    UnloadImage(scaled_image);
+									     
+  }
+}
+
+
+void create_gif_with_ffmeg(const char* output_file_name, int frame_delay) {
+
+  char command[512];
+  sprintf(command,
+	  "ffmpeg -y -framerate %d -i temp/frame_%%03d.png "
+	  "-vf \"scale=%d:%d:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse\" "
+	  "%s",
+	  100/frame_delay,
+	  GRID_SIZE * 64,
+	  GRID_SIZE * 64,
+	  output_file_name);
+
+  system(command);
+  TraceLog(LOG_INFO, "gif saved");
+  
+}
+
 
 int main (void)
 {
+  Color colors[MAX_COLOR_COUNT] = {
+    LIGHTGRAY,GRAY,DARKGRAY,YELLOW, GOLD, ORANGE, PINK, RED, MAROON, GREEN, LIME, DARKGREEN, SKYBLUE,BLUE, DARKBLUE, PURPLE, VIOLET, DARKPURPLE, BEIGE, BROWN, DARKBROWN, WHITE, BLACK, MAGENTA
+  };
 
   int color_selected = 0;
   int color_mouse_hover  = 0; // for later
@@ -75,13 +115,13 @@ int main (void)
     {
       Vector2 mouse_pos = GetMousePosition();
 
-      if(IsKeyPressed(KEY_PAGE_UP) && current_frame > 0) {
+      if(IsKeyPressed(KEY_A) && current_frame > 0) {
 	current_frame--;
 	
 	memcpy(grid, frames[current_frame].grid, sizeof(grid));
       }
 
-      if(IsKeyPressed(KEY_PAGE_DOWN) && current_frame < total_frames - 1){
+      if(IsKeyPressed(KEY_D) && current_frame < total_frames - 1){
 	current_frame++;
 
 	memcpy(grid, frames[current_frame].grid, sizeof(grid));
@@ -91,8 +131,14 @@ int main (void)
 	current_frame = total_frames;
 	total_frames++;
       }
+      if(IsKeyPressed(KEY_DELETE) && total_frames > 1){
+	current_frame = total_frames - 1;
+	total_frames--;
+      }
       if(IsKeyPressed(KEY_G)) {
 	//save_as_gif();
+	save_frames_as_pngs(frames, total_frames, CELL_SIZE);
+	create_gif_with_ffmeg("animate.gif", 20);
       }
       
       if(
@@ -150,7 +196,7 @@ int main (void)
 	{
 	  for(int x = 0; x < GRID_SIZE; x++)
 	    {
-	      DrawRectangle(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE, grid[x][y] );
+	      DrawRectangle(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE - 1 , CELL_SIZE -1 , grid[x][y] );
 
 	    }
       
@@ -171,7 +217,7 @@ int main (void)
 	}
 	
       }
-      
+
       DrawTextureRec(canvas.texture, (Rectangle){0, 0, (float)canvas.texture.width, (float)-canvas.texture.height},(Vector2){300, 0}, WHITE);
 
       DrawText(TextFormat("Frame: %d / %d", current_frame + 1, total_frames), 10, SCREEN_HEIGHT - 30, 20, WHITE);
